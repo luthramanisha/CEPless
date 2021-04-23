eae72b517918f8081ebc101453166d5c9c35bf30
unknown <manisha.luthra@kom.tu-darmstadt.de>
2021-04-23 19:46:09 +0200

    removed flink

--- flink/flink-streaming-java/src/main/java/org/apache/flink/streaming/api/customoperators/OperatorEventReceiver.java
+++
@@ -1,6 +0,0 @@
-package org.apache.flink.streaming.api.customoperators;
-
-public interface OperatorEventReceiver {
-	void receivedEvent(String event);
-}
-
--- flink/flink-streaming-java/src/main/java/org/apache/flink/streaming/api/customoperators/repository/EventRepository.java
+++
@@ -1,6 +0,0 @@
-package org.apache.flink.streaming.api.customoperators.repository;
-
-public interface EventRepository {
-	void listen(String addr);
-	void send(String addr, String item);
-}
